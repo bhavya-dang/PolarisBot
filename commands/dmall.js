a41025199ce9b81c30eb5eec2132b0d33e88168f
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-23 02:18:08 +0530

    Fixed bugs and made changes to embeds

---
+++ commands/dmall.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+module.exports.run = async(bot, message, args) => {
+    message.guild.members.map(async user => {
+        const msg = args.join(" ");
+        if(user.bot) return undefined;
+        if (msg.length < 1){
+            let noMessageEmbed = new Discord.RichEmbed()
+            .setTitle("**ERROR** - Please specify a message!")
+            .setThumbnail(message.guild.iconURL)
+            .addField("**Usage:**", "`p@dmall <message>`", true); 
+            return await message.channel.send(noMessageEmbed);
+        }else {
+            let embed = new Discord.RichEmbed()
+            .setTitle("Polaris Tournaments Announcement")
+            .setThumbnail(message.guild.iconURL)
+            .setColor("00ffc3")
+            .setDescription(msg)
+            .setTimestamp();
+            await user.send(embed);
+        }
+            });
+    };
+
+module.exports.help = {
+    name: "dmall"
+}
--- commands/roledm.js
+++ commands/roledm.js
@@ -1,16 +1,36 @@
 const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
-    message.guild.members.map(user => {
+    message.guild.members.map(async user => {
         // if(user.id == message.author.id || user.id == bot.user.id)
-        const role = message.guild.roles.find(r => r.name = args[0]);
+        const roleArgs = args.slice(0, 1);
+        const role = message.guild.roles.find(r => r.name == roleArgs.join(" "));
+        if (roleArgs < 1 || !role){ 
+           let errorEmbed = new Discord.RichEmbed()
+           .setTitle("**ERROR** - Please specify a valid role!")
+           .setThumbnail(message.guild.iconURL)
+           .addField("**Usage:**", "`p@roledm <rolename> <title> <message>`", true); 
+           return await message.channel.send(errorEmbed)
+        }else{
         if(user.roles.has(role.id)){
+            const messageArgs = args.slice(1);
+
+            if (messageArgs.length < 1){
+                let noMessageEmbed = new Discord.RichEmbed()
+                .setTitle("**ERROR** - Please specify a message!")
+                .setThumbnail(message.guild.iconURL)
+                .addField("**Usage:**", "`p@roledm <rolename> <message>`", true); 
+                return await message.channel.send(noMessageEmbed);
+            }else {
             let embed = new Discord.RichEmbed()
-            .setTitle("POLARISGG ~ Announcement!!!")
+            .setTitle("Polaris Tournaments Announcement")
             .setThumbnail(message.guild.iconURL)
             .setColor("00ffc3")
-            .setDescription(args[1]);
-            user.send(embed);
-        }else return;
+            .setDescription(messageArgs.join(" "))
+            .setTimestamp();
+            await user.send(embed);
+            }
+        }else return undefined;
+    }
     });
 };
 
