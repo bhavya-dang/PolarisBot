4858dc2836cad00a4a3ea9adc542f53d2046a76a
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-22 19:34:57 +0530

    4

--- commands/dm.js
+++ commands/dm.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
     message.guild.members.map(user => {
-        if(user.id == message.author.id || user.id == bot.user.id);
+        // if(user.id == message.author.id || user.id == bot.user.id);
             let embed = Discord.RichEmbed()
             .setTitle("POLARIS", message.guild.iconURL)
             .setColor("00ffc3")
--- commands/roledm.js
+++ commands/roledm.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
     message.guild.members.map(user => {
-        if(user.id == message.author.id || user.id == bot.user.id)
+        // if(user.id == message.author.id || user.id == bot.user.id)
         const role = message.guild.roles.find(`name`, args[0]);
         if(user.roles.has(role.id)){
             let embed = Discord.RichEmbed()
