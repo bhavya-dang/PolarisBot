9da12c36f3d9b9f56a4ce1ae2dd8b74051b4b0a5
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-23 12:03:07 +0530

    Fixed if loop/multiple messages

--- commands/dmall.js
+++ commands/dmall.js
@@ -1,29 +1,32 @@
 const Discord = require("discord.js");
-module.exports.run = async(bot, message, args) => {
-    message.guild.members.map(async user => {
-        if(message.author.id !== "299157627584118787" && message.author.id !== "414111663076147201"){ message.channel.send("**You can't use this command!**");
-    }else {
-        const msg = args.join(" ");
-        if(user.bot) return undefined;
-        if (msg.length < 1){
-            let noMessageEmbed = new Discord.RichEmbed()
-            .setTitle("**ERROR** - Please specify a message!")
-            .setThumbnail(message.guild.iconURL)
-            .addField("**Usage:**", "`p@dmall <message>`", true); 
-            return await message.channel.send(noMessageEmbed);
-        }else {
-            let embed = new Discord.RichEmbed()
-            .setTitle("Polaris Tournaments Announcement")
-            .setThumbnail(message.guild.iconURL)
-            .setColor("00ffc3")
-            .setDescription(msg)
-            .setTimestamp();
-            await user.send(embed);
-        }
-    }
-            });
-    };
+module.exports.run = async (bot, message, args) => {
+  await message.delete();
+
+  if (
+    message.author.id !== "299157627584118787" &&
+    message.author.id !== "414111663076147201"
+  )
+    return message.channel.send("**You can't use this command!**");
+
+  const msg = args.join(" ");
+  let noMessageEmbed = new Discord.RichEmbed()
+    .setTitle("**ERROR** - Please specify a message!")
+    .setThumbnail(message.guild.iconURL)
+    .addField("**Usage:**", "`p@dmall <message>`", true);
+
+  if (msg.length < 1) return message.channel.send(noMessageEmbed);
+  message.guild.members.map(async (user) => {
+    if (user.bot) return undefined;
+    let embed = new Discord.RichEmbed()
+      .setTitle("Polaris Tournaments Announcement")
+      .setThumbnail(message.guild.iconURL)
+      .setColor("00ffc3")
+      .setDescription(msg)
+      .setTimestamp();
+    await user.send(embed);
+  });
+};
 
 module.exports.help = {
-    name: "dmall"
-}
+  name: "dmall",
+};
--- commands/roledm.js
+++ commands/roledm.js
@@ -1,39 +1,52 @@
 const Discord = require("discord.js");
-module.exports.run = async(bot, message, args) => {
-    message.guild.members.map(async user => {
-        if(message.author.id !== "299157627584118787" || message.author.id !== "414111663076147201") return message.channel.send("**You can't use this command!**!");
-        const roleArgs = args.slice(0, 1);
-        const role = message.guild.roles.find(r => r.name == roleArgs.join(" "));
-        if (roleArgs < 1 || !role){ 
-           let errorEmbed = new Discord.RichEmbed()
-           .setTitle("**ERROR** - Please specify a valid role!")
-           .setThumbnail(message.guild.iconURL)
-           .addField("**Usage:**", "`p@roledm <rolename> <title> <message>`", true); 
-           return await message.channel.send(errorEmbed)
-        }else{
-        if(user.roles.has(role.id)){
-            const messageArgs = args.slice(1);
+module.exports.run = async (bot, message, args) => {
+  await message.delete();
 
-            if (messageArgs.length < 1){
-                let noMessageEmbed = new Discord.RichEmbed()
-                .setTitle("**ERROR** - Please specify a message!")
-                .setThumbnail(message.guild.iconURL)
-                .addField("**Usage:**", "`p@roledm <rolename> <message>`", true); 
-                return await message.channel.send(noMessageEmbed);
-            }else {
-            let embed = new Discord.RichEmbed()
+  const roleArgs = args.slice(0, 1);
+  let role = message.guild.member(
+    message.mentions.roles.first() ||
+      message.guild.roles.find((r) => r.name == roleArgs.join(" "))
+  );
+  if (
+    message.author.id !== "299157627584118787" &&
+    message.author.id !== "414111663076147201"
+  )
+    return message.channel.send("**You can't use this command!**!");
+  message.guild.members.map(async (user) => {
+    if (roleArgs < 1 || !role) {
+      let errorEmbed = new Discord.RichEmbed()
+        .setTitle("**ERROR** - Please specify a valid role!")
+        .setThumbnail(message.guild.iconURL)
+        .addField(
+          "**Usage:**",
+          "`p@roledm <rolename> <title> <message>`",
+          true
+        );
+      return await message.channel.send(errorEmbed);
+    } else {
+      if (user.roles.has(role.id)) {
+        const messageArgs = args.slice(1);
+
+        if (messageArgs.length < 1) {
+          let noMessageEmbed = new Discord.RichEmbed()
+            .setTitle("**ERROR** - Please specify a message!")
+            .setThumbnail(message.guild.iconURL)
+            .addField("**Usage:**", "`p@roledm <rolename> <message>`", true);
+          return await message.channel.send(noMessageEmbed);
+        } else {
+          let embed = new Discord.RichEmbed()
             .setTitle("Polaris Tournaments Announcement")
             .setThumbnail(message.guild.iconURL)
             .setColor("00ffc3")
             .setDescription(messageArgs.join(" "))
             .setTimestamp();
-            await user.send(embed);
-            }
-        }else return undefined;
+          await user.send(embed);
+        }
+      } else return undefined;
     }
-    });
+  });
 };
 
 module.exports.help = {
-    name: "roledm"
-}
+  name: "roledm",
+};
